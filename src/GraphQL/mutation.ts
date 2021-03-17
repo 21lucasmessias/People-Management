c6130902624f8b5050cee5487615d617b7bcef04
21.lucasmessias <21.lucasmessias@gmail.com>
2021-03-17 14:09:16 -0300

    RegisterPerson mutation implemented

--- src/GraphQL/mutation.ts
+++ src/GraphQL/mutation.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const GET_PERSONS = gql`
+export const REGISTER_PERSON = gql`
 mutation($name: NameInput!, $birthday: DateInput!, $cpf: String!, $rg: String!, $adress: AdressInput!) {
   registerPerson(
     name: $name,
